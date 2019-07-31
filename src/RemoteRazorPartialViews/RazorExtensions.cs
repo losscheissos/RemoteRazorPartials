fb244d1794231994a39230d1a7db03a96435cce5
Thorsten Reuther <thorsten.reuther@docuware.com>
2019-07-31 23:13:03 +0200

    Add Comment

--- src/RemoteRazorPartialViews/RazorExtensions.cs
+++ src/RemoteRazorPartialViews/RazorExtensions.cs
@@ -12,10 +12,25 @@ namespace Microsoft.AspNetCore.Mvc.Rendering
     {
         static HttpClient httpClient = new HttpClient();
 
+        /// <summary>
+        /// This method retrieves the HTML from the given URI and returns the HTML content, which can be placed
+        /// at the desired position using razor default handling
+        /// </summary>
+        /// <param name="htmlHelper">The HTML Helper instance</param>
+        /// <param name="remotePartialUri">The target URI as string</param>
+        /// <returns>Raw HtmlContent representation of the given web resource</returns>
         public static Task<IHtmlContent> RenderRemotePartialAsync(this IHtmlHelper htmlHelper, string remotePartialUri)
         {
             return RenderRemotePartialAsync(htmlHelper, new Uri(remotePartialUri));
         }
+
+        /// <summary>
+        /// This method retrieves the HTML from the given URI and returns the HTML content, which can be placed
+        /// at the desired position using razor default handling
+        /// </summary>
+        /// <param name="htmlHelper">The HTML Helper instance</param>
+        /// <param name="remotePartialUri">The target URI</param>
+        /// <returns>Raw HtmlContent representation of the given web resource</returns>
         public static async Task<IHtmlContent> RenderRemotePartialAsync(this IHtmlHelper htmlHelper, Uri remotePartialUri)
         {
             var result = await httpClient.GetAsync(remotePartialUri.AbsoluteUri);
